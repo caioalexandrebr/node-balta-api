b35c35d034288f4b7effda71ed524211ed94456b
Caio Alexandre <caioalexandre.br@gmail.com>
2019-03-27 13:22:41 -0300

    crud rest

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 let route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -11,6 +15,26 @@ let route = router.get('/', (req, res, next) => {
         version: '0.0.1'
     });
 });
+
+let create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+let put = router.put('/:id', (req, res, next) => {
+    let id = req.params.id;
+    res.status(200).send({ 
+        id: id,
+        item: req.body 
+    });
+});
+
+let del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
