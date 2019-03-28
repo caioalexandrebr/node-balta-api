7c97ba0b9313a7a21bce642d31ec93edc9106273
Caio Alexandre <caioalexandre.br@gmail.com>
2019-03-28 09:21:35 -0300

    mongoose

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+mongoose.connect('mongodb://localhost:27017/node_balta_api', {useNewUrlParser: true});
 
 // loading routes
 const index = require('./routes/index-route');
