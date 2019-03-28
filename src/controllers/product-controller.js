009d048ad09e4aab51786cf6491265003ac67416
Caio Alexandre <caioalexandre.br@gmail.com>
2019-03-28 13:13:19 -0300

    find by (all, slug, id and tags)

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,54 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active: true 
+        }, 'title prince slug')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true 
+        }, 'title description prince slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+
+exports.getByTag = (req, res, next) => {
+    Product
+        .find({
+            tags: req.params.tag,
+            active: true 
+        }, 'title description prince slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+
 exports.post = (req, res, next) => {
     let product = new Product();
     product.title = req.body.title;
@@ -14,23 +62,23 @@ exports.post = (req, res, next) => {
     product
         .save()
         .then(x => {
-            res.status(201).send({ 
-                message: 'Produto cadastrado com sucesso.' 
+            res.status(201).send({
+                message: 'Produto cadastrado com sucesso.'
             });
         })
         .catch(e => {
-            res.status(400).send({ 
-                message: 'Falha ao cadastrar o produto.', 
-                data: e 
+            res.status(400).send({
+                message: 'Falha ao cadastrar o produto.',
+                data: e
             });
         });
 };
 
 exports.put = (req, res, next) => {
     let id = req.params.id;
-    res.status(200).send({ 
+    res.status(200).send({
         id: id,
-        item: req.body 
+        item: req.body
     });
 };
 
