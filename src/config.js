21aaafd03ee3e5b5c880d26fe00e65f6caae8be7
Caio Alexandre <caioalexandre.br@gmail.com>
2019-05-09 10:53:48 -0300

    email sending service

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo internauta.'
+
 module.exports = {
-    connectionString: 'mongodb://localhost:27017/node_balta_api'
+    connectionString: 'mongodb://localhost:27017/node_balta_api',
+    sendgridKey: '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'
 }
